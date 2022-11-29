458e4b951a05aaf4c499c169464356b35fef8ca0
Nathan Mancheun Lui <nathanlui95@gmail.com>
2022-11-29 13:08:51 -0500

    retargeted

--- reward_loss.py
+++ reward_loss.py
@@ -1,65 +1,92 @@
-from mflow.utils.molecular_metrics import *
-from mflow.models.utils import construct_mol, construct_mol_with_validation
-
-
+import numpy as np
 from rdkit import Chem
 from rdkit.Contrib.SA_Score import sascorer as sa # SA Scoring from Ertl
 from rdkit.Contrib.NP_Score import npscorer as natp # natural product likeliness
 from rdkit.Chem import QED
 
+from mflow.utils.molecular_metrics import *
+from mflow.models.utils import construct_mol, construct_mol_with_validation
+from data import SparseMolecularDataset
+
+train_sparse= SparseMolecularDataset()
 fscore = natp.readNPModel()
 
-def synthetic_accessibility_total_score(sanitized_mols):
-    scores = list(map(sa.calculateScore,sanitized_mols))
-    return sum(scores)/len(scores)
+def normalize(data):
+    '''
+    data is a numpy array that gets normalized to [0, 1]
+    '''
+    if np.max(data) == np.min(data): return data / np.max(data)
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
 
-def natural_product_total_score(sanitized_mols):
-    scores = [natp.scoreMol(e, fscore) for e in sanitized_mols]
-    return sum(scores)/len(scores)
 
-def qed_total_score(sanitized_mols):
-    scores = list(map(QED.default,sanitized_mols))
-    return sum(scores)/len(scores)
+def synthetic_accessibility_scores(sanitized_mols):
+    scores = list(map(sa.calculateScore, sanitized_mols))  # [10, 1]
+    scores = np.array(list(map(lambda x: -10 if x is None else -x, scores)))  # [-10, -1]
+    return normalize(scores)  # [0, 1]
 
-def reward_loss(edges,nodes, atomic_num_list, training_data=None, weights = None):
+
+def natural_product_scores(sanitized_mols):
+    scores = np.array([natp.scoreMol(e, fscore) for e in sanitized_mols])
+    return normalize(scores)  # [0, 1]
+
+
+def qed_scores(sanitized_mols):
+    scores = np.array(list(map(QED.default, sanitized_mols)))
+    return normalize(scores)  # [0, 1]
+
+
+def drug_candidate_scores(logP, syn_acc, nov):
+    scores = (constant_bump(logP, 0.210, 0.945) + syn_acc + nov + (1 - nov) * 0.3) / 4
+    return normalize(scores)
+
+
+def constant_bump(x, x_low, x_high, decay=0.025):
+    return np.select(condlist=[x <= x_low, x >= x_high],
+                        choicelist=[np.exp(- (x - x_low) ** 2 / decay),
+                                    np.exp(- (x - x_high) ** 2 / decay)],
+                        default=np.ones_like(x))
+
+
+def reward_loss(edges, nodes, atomic_num_list, training_data=train_sparse, weights=None):
     """
-    weights is an iterable of numbers of length 7 corresponding to
-    [validity,uniqueness,novelty,drug_canditate_scores,synthetic_accessibility,diversity]
+    weights is an iterable of numbers of length 8 corresponding to
+    [np_score, water_octanol_partition, synthetic_accessibility, novelty, drug_candidacy, uniqueness, diversity, validity]
 
-    Returns sum of scores
+    Returns a batch-sized vector in which each entry is the product of the compounds individual scores
 
     """
-    adj = edges.__array__()  # , gpu)  (1000,4,9,9)
-    x = nodes.__array__()  # , gpu)  (1000,9,5)
+    adj = edges.__array__()  # , gpu)  (bs,4,9,9)
+    x = nodes.__array__()  # , gpu)  (bs,9,5)
+
     def to_mol(adj_x):
         adj_elem, x_elem = adj_x
         return construct_mol(x_elem, adj_elem, atomic_num_list)
     
-    def to_validated_mol(adj_x):
-        adj_elem, x_elem = adj_x
-        return construct_mol_with_validation(x_elem, adj_elem, atomic_num_list)
+    # def to_validated_mol(adj_x):
+    #     adj_elem, x_elem = adj_x
+    #     return construct_mol_with_validation(x_elem, adj_elem, atomic_num_list)
 
-    mols = list(map(to_mol,zip(adj,x)))
-    valid_mols = list(map(to_validated_mol,zip(adj,x)))
+    mols = list(map(to_mol, zip(adj, x)))
+    # valid_mols = list(map(to_validated_mol, zip(adj, x)))
     
-    sanitized_flags = list(map(Chem.SanitizeMol,valid_mols))
+    # sanitized_flags = list(map(Chem.SanitizeMol, valid_mols))
 
-    m = MolecularMetrics()
-    validity = m.valid_total_score(mols)
-    uniqueness = m.unique_total_score(mols)
-    #water_octanol_partition_coef = m.water_octanol_partition_coefficient_scores(mols).mean() # range -2.12178879609, 6.0429063424
-    synthetic_accessibility = synthetic_accessibility_total_score(valid_mols)
-    if training_data != None:
-        novelty = m.novel_total_score(mols, training_data)
-        drug_canditate_scores = m.drugcandidate_scores(valid_mols,training_data)
-    else:
-        novelty = 0
-        drug_canditate_scores = 0
-    diversity = m.diversity_scores(mols,training_data).mean()
+    mm = MolecularMetrics()
+    np_score = natural_product_scores(mols).reshape(-1,1)  # vec - [0, 1]
+    water_octanol_partition = mm.water_octanol_partition_coefficient_scores(mols, norm=True).reshape(-1,1) # vec - [0, 1]
+    synthetic_accessibility = synthetic_accessibility_scores(mols).reshape(-1,1)  # vec - [0, 1]
+    novelty = mm.novel_scores(mols, training_data).reshape(-1,1)  # vec - [0, 1]
+    drug_candidacy = drug_candidate_scores(water_octanol_partition, 
+        synthetic_accessibility, novelty).reshape(-1,1)  # vec - [0,1]
+    uniqueness = mm.unique_scores(mols).reshape(-1,1)  # vec - [0, 1]
+    diversity = mm.diversity_scores(mols, training_data).reshape(-1,1) # vec - [0, 1]
+    validity = mm.valid_scores(mols).reshape(-1,1)  # vec - [0, 1]
 
-    scores = [validity,uniqueness,novelty,drug_canditate_scores,synthetic_accessibility,diversity]
+    scores = np.hstack((np_score, water_octanol_partition, synthetic_accessibility, novelty, drug_candidacy, uniqueness, diversity, validity))
 
-    if weights == None:
-        return sum(scores)
-    else:
-        return sum([score*weight for score, weight in zip(scores, weights)])
+    if weights != None:
+        weights= np.array(weights).flatten()
+        weights= np.broadcast_to(weights, scores.shape)
+        scores = np.multiply(scores, weights)
+    
+    return scores.prod(axis=1).flatten()
