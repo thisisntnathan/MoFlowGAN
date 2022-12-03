db326556aa31a1b2f4b7211e022024c990a1eacd
Nathan Mancheun Lui <nathanlui95@gmail.com>
2022-12-03 18:30:30 -0500

    Removed uneeded function

--- data/smile_to_graph.py
+++ data/smile_to_graph.py
@@ -5,12 +5,6 @@ from rdkit import Chem
 from rdkit.Chem import rdmolops
 from rdkit.Chem.rdmolfiles import MolFromSmiles
 
-def smiles_to_nodes_edges(smiles):
-    mol = MolFromSmiles(smiles)
-    edges = construct_discrete_edge_matrix(mol)
-    nodes = construct_atomic_number_array(mol)
-    return nodes, edges
-
 
 class GGNNPreprocessor(object):
     """GGNN Preprocessor
